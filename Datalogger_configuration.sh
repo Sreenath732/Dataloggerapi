1a4769ffdaea095f574f4e8a7c41a57741ca476c
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-20 14:38:26 +0530

    Update Datalogger_configuration.sh

--- Datalogger_configuration.sh
+++ Datalogger_configuration.sh
@@ -398,7 +398,7 @@ select yn in "Yes" "No"; do
     sudo systemctl start dataloggerapi.service
     status7= $(sudo service dataloggerapi status)
     echo $status7
-    break;;
+    break
     esac
 done
 
