7fb515ade364431a0c48c2b1fe409bab0f3edf55
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-20 16:56:07 +0530

    Update copy_data.sh

--- copy_data.sh
+++ copy_data.sh
@@ -1,4 +1,4 @@
 
-PGPASSWORD=postgres sudo psql -h 127.0.0.1 -U postgres -d plc_data -p 5432  -c "\COPY data_table from '/home/circleci/project/data_table_10min.csv' DELIMITER ',' CSV HEADER;")
+PGPASSWORD=postgres sudo psql -h 127.0.0.1 -U postgres -d plc_data -p 5432  -c "\COPY data_table from '/home/circleci/project/data_table_10min.csv' DELIMITER ',' CSV HEADER;"
 
 
