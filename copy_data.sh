773256266c825365de4b4e551c036ae21a64921c
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-20 16:18:42 +0530

    Update copy_data.sh

--- copy_data.sh
+++ copy_data.sh
@@ -1,7 +1,4 @@
-status=$(sudo psql -h 127.0.0.1 -U postgres -d plc_data -p 5432  -c "\COPY data_table from '/home/circleci/project/data_table_10min.csv' DELIMITER ',' CSV HEADER;")
-
-
-read -p "Please enter postgres password to copy data into database: " postgres
-echo $postgres
-        
-        echo $status
+sudo -u postgres psql
+\c plc_data
+status= $(select \COPY data_table from '/home/sreenath/Downloads/data_table_10min.csv' DELIMITER ',' CSV HEADER;)
+echo $status
