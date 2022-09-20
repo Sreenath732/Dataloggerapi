3070af3bfd0d14ffc88c79c14a4c00e869b63831
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-20 16:25:21 +0530

    Update copy_data.sh

--- copy_data.sh
+++ copy_data.sh
@@ -1,4 +1,3 @@
-sudo -u postgres psql
-\c plc_data
-status= $(select \COPY data_table from '/home/sreenath/Downloads/data_table_10min.csv' DELIMITER ',' CSV HEADER;)
+status=$(sudo psql -h 127.0.0.1 -U postgres -d plc_data -p 5432  -c "\COPY data_table from '/home/circleci/project/data_table_10min.csv' DELIMITER ',' CSV HEADER;")
+
 echo $status
