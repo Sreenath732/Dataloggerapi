395cb9c950e75d7c2c163cbeb6ddaec5346c5c89
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-20 16:54:24 +0530

    Update copy_data.sh

--- copy_data.sh
+++ copy_data.sh
@@ -1,4 +1,4 @@
-set PGPASSWORD=root
-status=$(sudo psql -h 127.0.0.1 -U postgres -d plc_data -p 5432  -c "\COPY data_table from '/home/circleci/project/data_table_10min.csv' DELIMITER ',' CSV HEADER;")
 
-echo $status
+PGPASSWORD=postgres sudo psql -h 127.0.0.1 -U postgres -d plc_data -p 5432  -c "\COPY data_table from '/home/circleci/project/data_table_10min.csv' DELIMITER ',' CSV HEADER;")
+
+
