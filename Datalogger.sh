deceede5f2fe2a01609c9dc5e2a800afe6715715
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-21 11:08:50 +0530

    Update Datalogger.sh

--- Datalogger.sh
+++ Datalogger.sh
@@ -4,11 +4,10 @@ status1=$(./clientNew 127.0.0.1 6789 "[{\"username\":\"rushikesh\",\"passcode\":
 echo "get fields"
 echo $status1
 
-status2=$(./clientNew 127.0.0.1 6789 "[{\"username\":\"sateesh\",\"passcode\":\"123456\",\"authkey\":\"abcd\",\"endpoint\":\"fetchValues\",\"params\":[{\"start_time\":\"2022-09-01 12:30:00\",\"end_time\":\"2022-09-01 12:31:00\",\"skip\":\"600\",\"param_names\":[\"Header_Len\"]}]}]")
+status2=$(./clientNew 127.0.0.1 6789 "[{\"username\":\"sateesh\",\"passcode\":\"123456\",\"authkey\":\"abcd\",\"endpoint\":\"fetchValues\",\"params\":[{\"start_time\":\"2022-09-01 07:06:00\",\"end_time\":\"2022-09-01 07:07:00\",\"skip\":\"600\",\"param_names\":[\"Header_Len\"]}]}]")
 echo "fetch values"
 echo $status2
 
-status3=$(./clientNew 127.0.0.1 6789 "[{\"username\":\"sateesh\",\"passcode\":\"123456\",\"authkey\":\"abcd\",\"endpoint\":\"fetchTimestamps\",\"params\":[{\"start_time\":\"2022-09-01 12:30:00\",\"end_time\":\"2022-09-01 12:31:00\",\"param_names\":[{\"name\":\"A1_PD1\",\"operation\":\">\",\"value\":\"45\"}]}]")
+status3=$(./clientNew 127.0.0.1 6789 "[{\"username\":\"sateesh\",\"passcode\":\"123456\",\"authkey\":\"abcd\",\"endpoint\":\"fetchTimestamps\",\"params\":[{\"start_time\":\"2022-09-01 07:00:00\",\"end_time\":\"2022-09-01 07:10:00\",\"param_names\":[{\"name\":\"A1_PD1\",\"operation\":\">\",\"value\":\"45\"}]}]}]")
 echo "fetch timestamps"
 echo $status3
-
