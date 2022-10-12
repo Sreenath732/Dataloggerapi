3810d3d5edf469936b46a9e05c8414d4603fd7e6
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-10-12 12:21:38 +0530

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -5,3 +5,4 @@ sudo apt-get install libjsoncpp-dev
 sudo apt-get install libgcrypt-dev
 sree=$(g++ dataloggerapi.cpp.cpp -lpqxx -lpq -std=c++14 -o dataloggerapi -static-libstdc++ -ljsoncpp -lcrypt)
 echo $sree
+
